1799ae41465ad6b66da1d38c825118272e763ab3
Kimmo Forss <kimmo.forss@microsoft.com>
2020-09-06 15:58:31 +0300

    sap deployer to use naming module

--- deploy/terraform/bootstrap/sap_deployer/module.tf
+++ deploy/terraform/bootstrap/sap_deployer/module.tf
@@ -4,10 +4,30 @@ Description:
   Example to deploy deployer(s) using local backend.
 */
 module "sap_deployer" {
-  source         = "../../terraform-units/modules/sap_deployer"
-  infrastructure = var.infrastructure
-  deployers      = var.deployers
-  options        = var.options
-  ssh-timeout    = var.ssh-timeout
-  sshkey         = var.sshkey
+  source            = "../../terraform-units/modules/sap_deployer"
+  infrastructure    = var.infrastructure
+  deployers         = var.deployers
+  options           = var.options
+  ssh-timeout       = var.ssh-timeout
+  sshkey            = var.sshkey
+  prefix            = module.sap_namegenerator.prefixes["DEPLOYER"]
+  sa_prefix         = module.sap_namegenerator.sa_prefix
+  vm_names          = module.sap_namegenerator.vm_names["DEPLOYER"]
+  kv_names          = module.sap_namegenerator.kv_names["DEPLOYER"]
+  resource_suffixes = module.sap_namegenerator.resource_extensions
+}
+
+module "sap_namegenerator" {
+  source             = "../../terraform-units/modules/sap_namegenerator"
+  environment        = lower(try(var.infrastructure.landscape, ""))
+  location           = try(var.infrastructure.region, "")
+  codename           = lower(try(var.infrastructure.codename, ""))
+  deployer_vnet_name = try(var.infrastructure.vnets.management.name, "DEPLOYER")
+
+  //These are not needed for the deployer
+  sap_vnet_name = try(var.infrastructure.vnets.sap.name, "")
+  sap_sid       = ""
+  hdb_sid       = ""
+  random-id     = ""
+
 }
--- deploy/terraform/terraform-units/modules/sap_deployer/variables_local.tf
+++ deploy/terraform/terraform-units/modules/sap_deployer/variables_local.tf
@@ -4,46 +4,34 @@ Description:
   Define local variables.
 */
 
-variable "region_mapping" {
-  type        = map(string)
-  description = "Region Mapping: Full = Single CHAR, 4-CHAR"
-
-  # 28 Regions 
-
-  default = {
-    westus             = "weus"
-    westus2            = "wus2"
-    centralus          = "ceus"
-    eastus             = "eaus"
-    eastus2            = "eus2"
-    northcentralus     = "ncus"
-    southcentralus     = "scus"
-    westcentralus      = "wcus"
-    northeurope        = "noeu"
-    westeurope         = "weeu"
-    eastasia           = "eaas"
-    southeastasia      = "seas"
-    brazilsouth        = "brso"
-    japaneast          = "jpea"
-    japanwest          = "jpwe"
-    centralindia       = "cein"
-    southindia         = "soin"
-    westindia          = "wein"
-    uksouth2           = "uks2"
-    uknorth            = "ukno"
-    canadacentral      = "cace"
-    canadaeast         = "caea"
-    australiaeast      = "auea"
-    australiasoutheast = "ause"
-    uksouth            = "ukso"
-    ukwest             = "ukwe"
-    koreacentral       = "koce"
-    koreasouth         = "koso"
-  }
+variable prefix {
+  type        = string
+  description = "Resource naming prefix"
 }
 
+variable sa_prefix {
+  type        = string
+  description = "Storage account naming prefix"
+}
+
+variable vm_names {
+  type        = list
+  description = "Virtual machine name list"
+}
+
+variable kv_names {
+  type        = list
+  description = "Keyvault name list"
+}
+
+
+variable resource_suffixes {
+  type        = map
+  description = "List of resource suffixes"
+}
 // Set defaults
 locals {
+
 
   // Post fix for all deployed resources
   postfix = random_id.deployer.hex
@@ -54,26 +42,22 @@ locals {
   // Resource group and location
 
   region             = try(var.infrastructure.region, "")
-  landscape          = try(var.infrastructure.landscape, "")
-  location_short     = try(var.region_mapping[local.region], "unkn")
-  vnet_mgmt_tempname = try(local.vnet_mgmt.name, "deployer")
-  prefix             = try(var.infrastructure.resource_group.name, upper(format("%s-%s-%s", local.landscape, local.location_short, local.vnet_mgmt_tempname)))
-  sa_prefix          = lower(format("%s%s%sdiag", substr(local.landscape,0,5), local.location_short, substr(local.vnet_mgmt_tempname,0,7)))
-  rg_name            = try(var.infrastructure.resource_group.name,format("%s-INFRASTRUCTURE", local.prefix))
-
+  vnet_mgmt_tempname = try(local.vnet_mgmt.name, "DEPLOYER")
+  prefix             = try(var.infrastructure.resource_group.name, var.prefix)
+  rg_name            = try(var.infrastructure.resource_group.name, format("%s%s", local.prefix, var.resource_suffixes["deployer-rg"]))
 
   // Management vnet
   vnet_mgmt        = try(var.infrastructure.vnets.management, {})
   vnet_mgmt_exists = try(local.vnet_mgmt.is_existing, false)
   vnet_mgmt_arm_id = local.vnet_mgmt_exists ? try(local.vnet_mgmt.arm_id, "") : ""
-  vnet_mgmt_name   = local.vnet_mgmt_exists ? "" : try(local.vnet_mgmt.name, format("%s-vnet", local.prefix))
-  vnet_mgmt_addr   = local.vnet_mgmt_exists ? "" : try(local.vnet_mgmt.address_space, "10.0.0.0/24")
+  vnet_mgmt_name   = local.vnet_mgmt_exists ? "" : try(local.vnet_mgmt.name, format("%s%s", local.prefix, var.resource_suffixes["vnet"]))
+  vnet_mgmt_addr   = local.vnet_mgmt_exists ? "" : try(local.vnet_mgmt.address_space, "10.0.0.16/28")
 
   // Management subnet
   sub_mgmt          = try(local.vnet_mgmt.subnet_mgmt, {})
   sub_mgmt_exists   = try(local.sub_mgmt.is_existing, false)
   sub_mgmt_arm_id   = local.sub_mgmt_exists ? try(local.sub_mgmt.arm_id, "") : ""
-  sub_mgmt_name     = local.sub_mgmt_exists ? "" : try(local.sub_mgmt.name, format("%s_deployment-subnet", local.prefix))
+  sub_mgmt_name     = local.sub_mgmt_exists ? "" : try(local.sub_mgmt.name, format("%s%s", local.prefix, var.resource_suffixes["deployer-subnet"]))
   sub_mgmt_prefix   = local.sub_mgmt_exists ? "" : try(local.sub_mgmt.prefix, "10.0.0.16/28")
   sub_mgmt_deployed = try(local.sub_mgmt_exists ? data.azurerm_subnet.subnet_mgmt[0] : azurerm_subnet.subnet_mgmt[0], null)
 
@@ -81,7 +65,7 @@ locals {
   sub_mgmt_nsg             = try(local.sub_mgmt.nsg, {})
   sub_mgmt_nsg_exists      = try(local.sub_mgmt_nsg.is_existing, false)
   sub_mgmt_nsg_arm_id      = local.sub_mgmt_nsg_exists ? try(local.sub_mgmt_nsg.arm_id, "") : ""
-  sub_mgmt_nsg_name        = local.sub_mgmt_nsg_exists ? "" : try(local.sub_mgmt_nsg.name, format("%s_deploymentSubnet-nsg", local.prefix))
+  sub_mgmt_nsg_name        = local.sub_mgmt_nsg_exists ? "" : try(local.sub_mgmt_nsg.name, format("%s%s", local.prefix, var.resource_suffixes["deployer-subnet-nsg"]))
   deployer_pip_list        = azurerm_public_ip.deployer[*].ip_address
   sub_mgmt_nsg_allowed_ips = local.sub_mgmt_nsg_exists ? [] : try(concat(local.sub_mgmt_nsg.allowed_ips, local.deployer_pip_list), ["0.0.0.0/0"])
   sub_mgmt_nsg_deployed    = try(local.sub_mgmt_nsg_exists ? data.azurerm_network_security_group.nsg_mgmt[0] : azurerm_network_security_group.nsg_mgmt[0], null)
@@ -93,7 +77,7 @@ locals {
   enable_deployers = length(local.deployer_input) > 0 ? true : false
   deployers = [
     for idx, deployer in local.deployer_input : {
-      "name"                 = "deployer",
+      "name"                 = var.vm_names[idx],
       "destroy_after_deploy" = true,
       "size"                 = try(deployer.size, "Standard_D2s_v3"),
       "disk_type"            = try(deployer.disk_type, "StandardSSD_LRS")
