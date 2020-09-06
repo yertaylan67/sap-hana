ea9376668d031f7c809e709e5b3e008eeb48336d
Kimmo Forss <kimmo.forss@microsoft.com>
2020-09-06 20:15:00 +0300

    storage account name fix

--- deploy/terraform/terraform-units/modules/sap_namegenerator/resourcegroup.tf
+++ deploy/terraform/terraform-units/modules/sap_namegenerator/resourcegroup.tf
@@ -8,7 +8,7 @@ locals {
   sdu_sa_name      = lower(format("%s%s%sdiag%s", local.env_verified, local.location_short, local.vnet_verified, local.random-id_verified))
   library_sa_name  = lower(format("%s%s%ssaplib%s", local.env_verified, local.location_short, local.vnet_verified, local.random-id_verified))
   state_sa_name    = lower(format("%s%s%stfstate%s", local.env_verified, local.location_short, local.vnet_verified, local.random-id_verified))
-  deployer_sa_name = lower(format("%s%s%sdeploydiag%s", local.env_verified, local.location_short, local.dep_vnet_verified, local.random-id_verified))
-  vnet_sa_name     = lower(format("%s%s%sdeploydiag%s", local.env_verified, local.location_short, local.dep_vnet_verified, local.random-id_verified))
+  deployer_sa_name = lower(format("%s%s%sdiag%s", local.env_verified, local.location_short, local.dep_vnet_verified, local.random-id_verified))
+  vnet_sa_name     = lower(format("%s%s%sdiag%s", local.env_verified, local.location_short, local.dep_vnet_verified, local.random-id_verified))
 
 }
