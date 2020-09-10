7c8cae4c0f77a587dfccec6610544dfab91a4993
Kimmo Forss <kimmo.forss@microsoft.com>
2020-09-10 09:31:22 +0300

    change the vm naming for hana and any_db

--- deploy/terraform/terraform-units/modules/sap_namegenerator/vm.tf
+++ deploy/terraform/terraform-units/modules/sap_namegenerator/vm.tf
@@ -10,14 +10,13 @@ locals {
     format("%s%s%sdeploy%02d", lower(local.env_verified), local.location_short, local.dep_vnet_verified, idx)
   ]
 
-  anydb_server_names = flatten([[for idx in range(var.db_server_max_count) :
+  anydb_server_names = [for idx in range(var.db_server_max_count) :
     format("%sd%s%02dl%d%s", lower(var.sap_sid), lower(var.db_sid), idx, 0, local.random-id_verifiedvm)
-    ],
-    [for idx in range(var.db_server_max_count) :
+    ]
+
+  anydb_server_names_ha =  [for idx in range(var.db_server_max_count) :
       format("%sd%s%02dl%d%s", lower(var.sap_sid), lower(var.db_sid), idx, 1, local.random-id_verifiedvm)
     ]
-    ]
-  )
 
   app_server_names = [for idx in range(var.app_server_max_count) :
     format("%sapp%02d%s%s", lower(var.sap_sid), idx, local.app_oscode, local.random-id_verifiedvm)
@@ -27,15 +26,13 @@ locals {
     format("%s%s%siscsi%02d", lower(local.env_verified), local.vnet_verified, local.location_short , idx)
   ]
 
-
-{environment[_map]}{sap_vnet}{region_map}iscsi##
-  hana_server_names = flatten([[for idx in range(var.db_server_max_count) :
+  hana_server_names = [for idx in range(var.db_server_max_count) :
     format("%sd%s%02dl%d%s", lower(var.sap_sid), lower(var.db_sid), idx, 0, local.random-id_verifiedvm)
-    ],
-    [for idx in range(var.db_server_max_count) :
+    ]
+
+  hana_server_names_ha = [for idx in range(var.db_server_max_count) :
       format("%sd%s%02dl%d%s", lower(var.sap_sid), lower(var.db_sid), idx, 1, local.random-id_verifiedvm)
     ]
-  ])
 
   scs_server_names = [for idx in range(var.app_server_max_count) :
     format("%sscs%02d%s%s", lower(var.sap_sid), idx, local.app_oscode, local.random-id_verifiedvm)
