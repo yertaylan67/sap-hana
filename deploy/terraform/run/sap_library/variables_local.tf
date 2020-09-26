c191822d73f0c462cd2ec067ec40e4214b58f0d8
Kimmo Forss <kimmo.forss@microsoft.com>
2020-09-26 11:44:06 +0300

    Delete variables_local.tf

--- deploy/terraform/run/sap_library/variables_local.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  environment          = lower(try(var.infrastructure.landscape, ""))
-  location             = try(var.infrastructure.region, "")
-  codename             = lower(try(var.infrastructure.codename, ""))
-  management_vnet_name = try(var.infrastructure.vnets.management.name, "MGMT")
-}
