12ae142089a1a82bcfef0127f70c868cb7e2dc80
Kimmo Forss <kimmo.forss@microsoft.com>
2020-10-02 08:54:00 +0300

    rebasing

--- deploy/terraform/bootstrap/sap_library/module.tf
+++ deploy/terraform/bootstrap/sap_library/module.tf
@@ -2,7 +2,7 @@
   Description:
   Setup sap library
 */
-module sap_library {
+module "sap_library" {
   source                  = "../../terraform-units/modules/sap_library"
   infrastructure          = var.infrastructure
   storage_account_sapbits = var.storage_account_sapbits
@@ -16,18 +16,3 @@ module "sap_namegenerator" {
   location    = try(var.infrastructure.region, "")
   random_id   = module.sap_library.random_id
 }
-
-module sap_namegenerator {
-  source      = "../../terraform-units/modules/sap_namegenerator"
-  environment = lower(try(var.infrastructure.landscape, ""))
-  location    = try(var.infrastructure.region, "")
-  random-id   = random_id.lib-random-id.hex
-
-  //These are not needed for the library
-  codename             = ""
-  management_vnet_name = ""
-  sap_vnet_name        = ""
-  sap_sid              = ""
-  db_sid               = ""
-}
-
