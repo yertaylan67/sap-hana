0dcfa4b2afb965b881c0acffb7fe7367185e80b6
Kimmo Forss <kimmo.forss@microsoft.com>
2020-10-10 02:37:11 +0300

    create avsets per zone

--- deploy/terraform/terraform-units/modules/sap_system/anydb_node/infrastructure.tf
+++ deploy/terraform/terraform-units/modules/sap_system/anydb_node/infrastructure.tf
@@ -47,7 +47,7 @@ resource "azurerm_network_interface_backend_address_pool_association" "anydb" {
 
 resource "azurerm_availability_set" "anydb" {
   count                        = local.enable_deployment ?  max(length(local.zones),1) : 0
-  name                         = format("%s%s", local.prefix, local.resource_suffixes.db-avset)
+  name                         = local.zonal_deployment ? format("%s_z%s%s", local.prefix, local.zones[count.index], local.resource_suffixes.db-avset) : format("%s%s", local.prefix, local.resource_suffixes.db-avset)
   location                     = var.resource-group[0].location
   resource_group_name          = var.resource-group[0].name
   platform_update_domain_count = 20
--- deploy/terraform/terraform-units/modules/sap_system/app_tier/infrastructure.tf
+++ deploy/terraform/terraform-units/modules/sap_system/app_tier/infrastructure.tf
@@ -112,7 +112,7 @@ resource "azurerm_lb_rule" "ers" {
 # Create the SCS Availability Set
 resource "azurerm_availability_set" "scs" {
   count                        = local.enable_deployment ? max(length(local.zones), 1) : 0
-  name                         = format("%s%s", local.prefix, local.resource_suffixes.scs-avset)
+  name                         = local.zonal_deployment ? format("%s_z%s%s", local.prefix, local.zones[count.index], local.resource_suffixes.scs-avset) : format("%s%s", local.prefix, local.resource_suffixes.scs-avset)
   location                     = var.resource-group[0].location
   resource_group_name          = var.resource-group[0].name
   platform_update_domain_count = 20
@@ -128,7 +128,7 @@ resource "azurerm_availability_set" "scs" {
 # Create the Application Availability Set
 resource "azurerm_availability_set" "app" {
   count                        = local.enable_deployment ? max(length(local.zones), 1) : 0
-  name                         = format("%s%s", local.prefix, local.resource_suffixes.app-avset)
+  name                         = local.zonal_deployment ? format("%s_z%s%s", local.prefix, local.zones[count.index], local.resource_suffixes.app-avset) : format("%s%s", local.prefix, local.resource_suffixes.app-avset)
   location                     = var.resource-group[0].location
   resource_group_name          = var.resource-group[0].name
   platform_update_domain_count = 20
@@ -192,7 +192,7 @@ resource "azurerm_network_interface_backend_address_pool_association" "web" {
 # Create the Web dispatcher Availability Set
 resource "azurerm_availability_set" "web" {
   count                        = local.enable_deployment ? max(length(local.zones), 1) : 0
-  name                         = format("%s%s", local.prefix, local.resource_suffixes.web-avset)
+  name                         = local.zonal_deployment ? format("%s_z%s%s", local.prefix, local.zones[count.index], local.resource_suffixes.web-avset) : format("%s%s", local.prefix, local.resource_suffixes.web-avset)
   location                     = var.resource-group[0].location
   resource_group_name          = var.resource-group[0].name
   platform_update_domain_count = 20
