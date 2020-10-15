c1424b5c7388f662ec849f7e4a9fab7b55984ce7
Kimmo Forss <kimmo.forss@microsoft.com>
2020-10-16 02:53:13 +0300

    formatting

--- deploy/terraform/terraform-units/modules/sap_system/anydb_node/variables_local.tf
+++ deploy/terraform/terraform-units/modules/sap_system/anydb_node/variables_local.tf
@@ -30,7 +30,7 @@ locals {
   disk_sizes = "${path.module}/../../../../../configs/anydb_sizes.json"
   sizes      = jsondecode(file(length(var.custom_disk_sizes_filename) > 0 ? var.custom_disk_sizes_filename : local.disk_sizes))
 
-  computer_names       = var.naming.virtualmachine_names.ANYDB_COMPUTERNAME 
+  computer_names       = var.naming.virtualmachine_names.ANYDB_COMPUTERNAME
   virtualmachine_names = var.naming.virtualmachine_names.ANYDB_VMNAME
 
   storageaccount_names = var.naming.storageaccount_names.SDU
--- deploy/terraform/terraform-units/modules/sap_system/hdb_node/variables_local.tf
+++ deploy/terraform/terraform-units/modules/sap_system/hdb_node/variables_local.tf
@@ -38,7 +38,7 @@ locals {
   disk_sizes = "${path.module}/../../../../../configs/hdb_sizes.json"
   sizes      = jsondecode(file(length(var.custom_disk_sizes_filename) > 0 ? var.custom_disk_sizes_filename : local.disk_sizes))
 
-  computer_names       = var.naming.virtualmachine_names.HANA_COMPUTERNAME 
+  computer_names       = var.naming.virtualmachine_names.HANA_COMPUTERNAME
   virtualmachine_names = var.naming.virtualmachine_names.HANA_VMNAME
 
   storageaccount_names = var.naming.storageaccount_names.SDU
