1a7c33a56442adb1e2ca659781a4c52f93fe4a65
Kimmo Forss <kimmo.forss@microsoft.com>
2020-10-18 00:50:56 +0300

    add zonality to anchor name

--- deploy/terraform/terraform-units/modules/sap_namegenerator/vm.tf
+++ deploy/terraform/terraform-units/modules/sap_namegenerator/vm.tf
@@ -2,11 +2,11 @@ locals {
 
   db_oscode       = upper(var.db_ostype) == "LINUX" ? "l" : "w"
   app_oscode      = upper(var.app_ostype) == "LINUX" ? "l" : "w"
-  anchor_oscode = upper(anchor_ostype) == "LINUX" ? "l" : "w"
+  anchor_oscode   = upper(var.anchor_ostype) == "LINUX" ? "l" : "w"
   db_platformcode = substr(var.db_platform, 0, 3)
 
   anchor_server_names = [for idx in range(length(var.zones)) :
-    format("%sanchor%02d%s%s", lower(var.sap_sid), idx, local.anchor_oscode, local.random_id_vm_verified)
+    format("%sanchor_z%s_%02d%s%s", lower(var.sap_sid), var.zones[idx % length(var.zones)], idx, local.anchor_oscode, local.random_id_vm_verified)
   ]
 
   deployer_vm_names = [for idx in range(var.deployer_vm_count) :
