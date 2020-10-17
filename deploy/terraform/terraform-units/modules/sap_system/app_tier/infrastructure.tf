61431bb10c8858d7aaf3abc82c88fc21f4990e96
Kimmo Forss <kimmo.forss@microsoft.com>
2020-10-17 03:22:37 +0300

    apptier_dual_nics

--- deploy/terraform/terraform-units/modules/sap_system/app_tier/infrastructure.tf
+++ deploy/terraform/terraform-units/modules/sap_system/app_tier/infrastructure.tf
@@ -1,6 +1,6 @@
 # Creates admin subnet of SAP VNET
 resource "azurerm_subnet" "sap-admin" {
-  count                = local.enable_deployment && local.anydb_dual_nics ? (local.sub_admin_exists ? 0 : 1) : 0
+  count                = local.enable_deployment && local.apptier_dual_nics ? (local.sub_admin_exists ? 0 : 1) : 0
   name                 = local.sub_admin_name
   resource_group_name  = var.vnet-sap[0].resource_group_name
   virtual_network_name = var.vnet-sap[0].name
@@ -9,7 +9,7 @@ resource "azurerm_subnet" "sap-admin" {
 
 # Imports data of existing SAP admin subnet
 data "azurerm_subnet" "sap-admin" {
-  count                = local.enable_deployment  && local.anydb_dual_nics ? (local.sub_admin_exists ? 1 : 0) : 0
+  count                = local.enable_deployment  && local.apptier_dual_nics ? (local.sub_admin_exists ? 1 : 0) : 0
   name                 = split("/", local.sub_admin_arm_id)[10]
   resource_group_name  = split("/", local.sub_admin_arm_id)[4]
   virtual_network_name = split("/", local.sub_admin_arm_id)[8]
