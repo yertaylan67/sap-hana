08ac35b705d55cbc99dcf93030927353a5b1df11
Kimmo Forss <kimmo.forss@microsoft.com>
2020-10-19 12:02:39 +0300

    removed whitespace

--- deploy/terraform/run/sap_system/variables_local.tf
+++ deploy/terraform/run/sap_system/variables_local.tf
@@ -89,7 +89,6 @@ locals {
   webdispatcher_count = try(var.application.webdispatcher_count, 0)
   scs_server_count    = try(var.application.scs_high_availability, false) ? 2 : 1
 
-
   db_zones  = try(var.databases[0].zones, [])
   app_zones = try(var.application.app_zones, [])
   scs_zones = try(var.application.scs_zones, [])
