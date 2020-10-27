5c329b3323ce21ed33253a84fe63a06ca8a38d53
Kimmo Forss <kimmo.forss@microsoft.com>
2020-10-27 11:23:52 +0200

    Output the IP Addresses for DNS

--- deploy/terraform/terraform-units/modules/sap_system/anydb_node/outputs.tf
+++ deploy/terraform/terraform-units/modules/sap_system/anydb_node/outputs.tf
@@ -2,6 +2,18 @@ output "nics-anydb" {
   value = azurerm_network_interface.anydb_db
 }
 
+output "anydb_admin_ip" {
+  value = azurerm_network_interface.anydb_admin.ip_configuration.private_ip_address
+}
+
+output "anydb_db_ip" {
+  value = azurerm_network_interface.anydb_db.ip_configuration.private_ip_address
+}
+
+output "anydb_lb_ip" {
+  value = azurerm_lb.anydb.frontend_ip_configuration.private_ip_address
+}
+
 output "any-database-info" {
   value = try(local.enable_deployment ? local.anydb_database : map(false), {})
 }
--- deploy/terraform/terraform-units/modules/sap_system/app_tier/outputs.tf
+++ deploy/terraform/terraform-units/modules/sap_system/app_tier/outputs.tf
@@ -9,3 +9,39 @@ output "nics-app" {
 output "nics-web" {
   value = azurerm_network_interface.web
 }
+
+output "app_ip" {
+  value = azurerm_network_interface.app.ip_configuration.private_ip_address
+}
+
+output "app_admin_ip" {
+  value = azurerm_network_interface.app-admin.ip_configuration.private_ip_address
+}
+
+output "scs_ip" {
+  value = azurerm_network_interface.scs.ip_configuration.private_ip_address
+}
+
+output "scs_admin_ip" {
+  value = azurerm_network_interface.scs-admin.ip_configuration.private_ip_address
+}
+
+output "scs_ip" {
+  value = azurerm_network_interface.scs.ip_configuration.private_ip_address
+}
+
+output "scs_admin_ip" {
+  value = azurerm_network_interface.scs-admin.ip_configuration.private_ip_address
+}
+
+output "web_lb_ip" {
+  value =  azurerm_lb.web.frontend_ip_configuration.private_ip_address
+}
+
+output "scs_lb_ip" {
+  value =  azurerm_lb.scs.frontend_ip_configuration[0].private_ip_address
+}
+
+output "ers_lb_ip" {
+  value =  azurerm_lb.web.frontend_ip_configuration[1].private_ip_address
+}
