ffea65d4950d50ef1b06e340f0380ea174164e1b
Kimmo Forss <kimmo.forss@microsoft.com>
2020-10-27 11:39:14 +0200

    multiple returns

--- deploy/terraform/terraform-units/modules/sap_system/app_tier/outputs.tf
+++ deploy/terraform/terraform-units/modules/sap_system/app_tier/outputs.tf
@@ -11,37 +11,37 @@ output "nics-web" {
 }
 
 output "app_ip" {
-  value = azurerm_network_interface.app.ip_configuration.private_ip_address
+  value = azurerm_network_interface.app.*.ip_configuration.private_ip_address
 }
 
 output "app_admin_ip" {
-  value = azurerm_network_interface.app-admin.ip_configuration.private_ip_address
+  value = azurerm_network_interface.app-admin.*.ip_configuration.private_ip_address
 }
 
 output "scs_ip" {
-  value = azurerm_network_interface.scs.ip_configuration.private_ip_address
+  value = azurerm_network_interface.scs.*.ip_configuration.private_ip_address
 }
 
 output "scs_admin_ip" {
-  value = azurerm_network_interface.scs-admin.ip_configuration.private_ip_address
+  value = azurerm_network_interface.scs-admin.*.ip_configuration.private_ip_address
 }
 
 output "web_ip" {
-  value = azurerm_network_interface.web.ip_configuration.private_ip_address
+  value = azurerm_network_interface.web.*.ip_configuration.private_ip_address
 }
 
 output "web_admin_ip" {
-  value = azurerm_network_interface.web-admin.ip_configuration.private_ip_address
+  value = azurerm_network_interface.web-admin.*.ip_configuration.private_ip_address
 }
 
 output "web_lb_ip" {
-  value =  azurerm_lb.web.frontend_ip_configuration.private_ip_address
+  value =  azurerm_lb.web.*.frontend_ip_configuration.private_ip_address
 }
 
 output "scs_lb_ip" {
-  value =  azurerm_lb.scs.frontend_ip_configuration[0].private_ip_address
+  value =  azurerm_lb.scs.*.frontend_ip_configuration[0].private_ip_address
 }
 
 output "ers_lb_ip" {
-  value =  azurerm_lb.web.frontend_ip_configuration[1].private_ip_address
+  value =  azurerm_lb.web.*.frontend_ip_configuration[1].private_ip_address
 }
