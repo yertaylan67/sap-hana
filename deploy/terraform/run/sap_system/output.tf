a999c7ea9dac758152b32bcc63674419ad984727
Kimmo Forss <kimmo.forss@microsoft.com>
2020-10-27 15:28:41 +0200

    consolidaed output

--- deploy/terraform/run/sap_system/output.tf
+++ deploy/terraform/run/sap_system/output.tf
@@ -1,8 +1,8 @@
 // Output IP Addresses
-
+/*
 output "anydb_vm_names" {
   value = module.anydb_node.anydb_vm_names
-  }
+}
 
 output "anydb_computer_names" {
   value = module.anydb_node.anydb_computer_names
@@ -50,15 +50,15 @@ output "web_admin_ip" {
 }
 
 output "web_lb_ip" {
-  value =  module.app_tier.web_lb_ip
+  value = module.app_tier.web_lb_ip
 }
 
 output "scs_lb_ip" {
-  value =  module.app_tier.scs_lb_ip
+  value = module.app_tier.scs_lb_ip
 }
 
 output "ers_lb_ip" {
-  value =  module.app_tier.ers_lb_ip
+  value = module.app_tier.ers_lb_ip
 }
 
 output "app_vm_names" {
@@ -91,4 +91,71 @@ output "scs_lb_name" {
 
 output "web_lb_name" {
   value = module.app_tier.web_lb_name
+}
+*/
+
+output "dns_info" {
+  value = concat(
+    flatten([for idx, vm in module.anydb_node.anydb_vm_names :
+      {
+        "${vm}" = module.anydb_node.anydb_db_ip[idx]
+      }
+    ]),
+    flatten([for idx, vm in module.anydb_node.anydb_vm_names :
+      {
+        "${vm}" = module.anydb_node.anydb_admin_ip[idx]
+      }
+    ]),
+    flatten([for idx, vm in module.app_tier.app_vm_names :
+      {
+        "${vm}" = module.app_tier.app_ip[idx]
+      }
+    ]),
+    flatten([for idx, vm in module.app_tier.app_vm_names :
+      {
+        "${vm}" = module.app_tier.app_admin_ip[idx]
+      }
+    ]),
+    flatten([for idx, vm in module.app_tier.scs_vm_names :
+      {
+        "${vm}" = module.app_tier.scs_ip[idx]
+      }
+    ]),
+    flatten([for idx, vm in module.app_tier.scs_vm_names :
+      {
+        "${vm}" = module.app_tier.scs_admin_ip[idx]
+      }
+    ]),
+    flatten([for idx, vm in module.app_tier.web_vm_names :
+      {
+        "${vm}" = module.app_tier.web_ip[idx]
+      }
+    ]),
+    flatten([for idx, vm in module.app_tier.web_vm_names :
+      {
+        "${vm}" = module.app_tier.web_admin_ip[idx]
+      }
+    ]),
+    flatten([for idx, lb in module.anydb_node.db_lb_name :
+      {
+        "${lb}" = module.anydb_node.anydb_lb_ip[idx]
+      }
+    ]),
+    flatten([for idx, lb in module.app_tier.scs_lb_name :
+      {
+        "${lb}" = module.app_tier.scs_lb_ip[idx]
+      }
+    ]),
+    flatten([for idx, lb in module.app_tier.scs_lb_name :
+      {
+        "${lb}" = module.app_tier.ers_lb_ip[idx]
+      }
+    ]),
+    flatten([for idx, lb in module.app_tier.web_lb_name :
+      {
+        "${lb}" = module.app_tier.web_lb_ip[idx]
+      }
+    ])
+
+  )
 }
