e4251b1b38d093824fc6d2d5a9a1cb651bb23b9f
Kimmo Forss <kimmo.forss@microsoft.com>
2020-10-27 11:46:29 +0200

    anydb outputs

--- deploy/terraform/run/sap_system/output.tf
+++ deploy/terraform/run/sap_system/output.tf
@@ -1,13 +1,13 @@
 // Output IP Addresses
 
 output "anydb_admin_ip" {
-  value = azurerm_network_interface.anydb_admin.*.ip_configuration.private_ip_address
+  value = module.anydb_node.anydb_admin_ip
 }
 
 output "anydb_db_ip" {
-  value = azurerm_network_interface.anydb_db.*.ip_configuration.private_ip_address
+  value = module.anydb_node.anydb_db_ip
 }
 
 output "anydb_lb_ip" {
-  value = azurerm_lb.anydb.*.frontend_ip_configuration.private_ip_address
+  value = module.anydb_node.anydb_db_ip.anydb_lb_ip
 }
