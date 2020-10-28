4b31a92bcf58e2cd3a6530cf051c24c123ab3611
Kimmo Forss <kimmo.forss@microsoft.com>
2020-10-29 01:05:49 +0200

    only create admin subment if needed

--- deploy/terraform/terraform-units/modules/sap_system/common_infrastructure/variables_local.tf
+++ deploy/terraform/terraform-units/modules/sap_system/common_infrastructure/variables_local.tf
@@ -84,6 +84,8 @@ locals {
   //iSCSI
   var_iscsi = try(local.var_infra.iscsi, {})
 
+  create_admin_subnet = try(var.application.dual_nics, false) || try(local.anydb.dual_nics, false) || length(local.hana-databases) > 0
+
   //iSCSI target device(s) is only created when below conditions met:
   //- iscsi is defined in input JSON
   //- AND
