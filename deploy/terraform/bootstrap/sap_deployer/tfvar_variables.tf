eedfe9c0b1812847ce78db74fe6dfe5353b38458
rukawata <rukawata@gmail.com>
2020-11-12 14:14:10 -0800

    change to use pass tfvar with dict in deployer

--- deploy/terraform/bootstrap/sap_deployer/tfvar_variables.tf
+++ deploy/terraform/bootstrap/sap_deployer/tfvar_variables.tf
@@ -1,3 +1,30 @@
+locals {
+  deployer_tfvars = {
+    location                            = local.location
+    deployer_vnet_address_space         = var.deployer_vnet_address_space
+    deployer_subnet_prefix              = var.deployer_subnet_prefix
+    deployer_rg_name                    = var.deployer_rg_name
+    vnet_mgmt_arm_id                    = var.vnet_mgmt_arm_id
+    vnet_mgmt_name                      = var.vnet_mgmt_name
+    sub_mgmt_arm_id                     = var.sub_mgmt_arm_id
+    sub_mgmt_name                       = var.sub_mgmt_name
+    sub_mgmt_nsg_arm_id                 = var.sub_mgmt_nsg_arm_id
+    sub_mgmt_nsg_name                   = var.sub_mgmt_nsg_name
+    sub_mgmt_nsg_allowed_ips            = var.sub_mgmt_nsg_allowed_ips
+    deployer_size                       = var.deployer_size
+    deployer_disk_type                  = var.deployer_disk_type
+    deployer_os_source_image_id         = var.deployer_os_source_image_id
+    deployer_os                         = var.deployer_os
+    deployer_authentication_type        = var.deployer_authentication_type
+    deployer_authentication_username    = var.deployer_authentication_username
+    deployer_authentication_password    = var.deployer_authentication_password
+    deployer_sshkey_path_to_public_key  = var.deployer_sshkey_path_to_public_key
+    deployer_sshkey_path_to_private_key = var.deployer_sshkey_path_to_private_key
+    deployer_private_ip_address         = var.deployer_private_ip_address
+    enable_secure_transfer              = var.enable_secure_transfer
+  }
+}
+
 // tfvar variables
 variable "deployer_rg_name" {
   default = ""
--- deploy/terraform/terraform-units/modules/sap_deployer/variables_local.tf
+++ deploy/terraform/terraform-units/modules/sap_deployer/variables_local.tf
@@ -7,6 +7,9 @@ variable naming {
   description = "naming convention"
 }
 
+// tfvar interface
+variable "deployer_tfvars" {}
+
 // Set defaults
 locals {
 
@@ -16,14 +19,14 @@ locals {
   resource_suffixes    = var.naming.resource_suffixes
 
   // Default option(s):
-  enable_secure_transfer = try(var.options.enable_secure_transfer, var.enable_secure_transfer)
+  enable_secure_transfer = try(var.options.enable_secure_transfer, var.deployer_tfvars.enable_secure_transfer)
 
   // Resource group and location
-  region = try(var.infrastructure.region, var.location)
+  region = try(var.infrastructure.region, var.deployer_tfvars.location)
   prefix = try(var.infrastructure.resource_group.name, var.naming.prefix.DEPLOYER)
   rg_name = try(
     var.infrastructure.resource_group.name,
-    var.deployer_rg_name != "" ? var.deployer_rg_name : format("%s%s", local.prefix, local.resource_suffixes.deployer_rg)
+    var.deployer_tfvars.deployer_rg_name != "" ? var.deployer_tfvars.deployer_rg_name : format("%s%s", local.prefix, local.resource_suffixes.deployer_rg)
   )
 
 
@@ -32,36 +35,36 @@ locals {
 
   // Management vnet
   vnet_mgmt        = try(var.infrastructure.vnets.management, {})
-  vnet_mgmt_arm_id = try(local.vnet_mgmt.arm_id, var.vnet_mgmt_arm_id)
+  vnet_mgmt_arm_id = try(local.vnet_mgmt.arm_id, var.deployer_tfvars.vnet_mgmt_arm_id)
   vnet_mgmt_exists = length(local.vnet_mgmt_arm_id) > 0 ? true : false
   vnet_mgmt_name = local.vnet_mgmt_exists ? split("/", local.vnet_mgmt_arm_id)[8] : try(
     local.vnet_mgmt.name,
-    var.vnet_mgmt_name != "" ? var.vnet_mgmt_name : format("%s-vnet", local.prefix)
+    var.deployer_tfvars.vnet_mgmt_name != "" ? var.deployer_tfvars.vnet_mgmt_name : format("%s-vnet", local.prefix)
   )
-  vnet_mgmt_addr = local.vnet_mgmt_exists ? "" : try(local.vnet_mgmt.address_space, var.deployer_vnet_address_space)
+  vnet_mgmt_addr = local.vnet_mgmt_exists ? "" : try(local.vnet_mgmt.address_space, var.deployer_tfvars.deployer_vnet_address_space)
 
   // Management subnet
   sub_mgmt        = try(local.vnet_mgmt.subnet_mgmt, {})
-  sub_mgmt_arm_id = try(local.sub_mgmt.arm_id, var.sub_mgmt_arm_id)
+  sub_mgmt_arm_id = try(local.sub_mgmt.arm_id, var.deployer_tfvars.sub_mgmt_arm_id)
   sub_mgmt_exists = length(local.sub_mgmt_arm_id) > 0 ? true : false
   sub_mgmt_name = local.sub_mgmt_exists ? split("/", local.sub_mgmt_arm_id)[10] : try(
     local.sub_mgmt.name,
-    var.sub_mgmt_name != "" ? var.sub_mgmt_name : format("%s_deployment-subnet", local.prefix)
+    var.deployer_tfvars.sub_mgmt_name != "" ? var.deployer_tfvars.sub_mgmt_name : format("%s_deployment-subnet", local.prefix)
   )
-  sub_mgmt_prefix   = local.sub_mgmt_exists ? "" : try(local.sub_mgmt.prefix, var.deployer_subnet_prefix)
+  sub_mgmt_prefix   = local.sub_mgmt_exists ? "" : try(local.sub_mgmt.prefix, var.deployer_tfvars.deployer_subnet_prefix)
   sub_mgmt_deployed = try(local.sub_mgmt_exists ? data.azurerm_subnet.subnet_mgmt[0] : azurerm_subnet.subnet_mgmt[0], null)
 
   // Management NSG
   sub_mgmt_nsg        = try(local.sub_mgmt.nsg, {})
-  sub_mgmt_nsg_arm_id = try(local.sub_mgmt_nsg.arm_id, var.sub_mgmt_nsg_arm_id)
+  sub_mgmt_nsg_arm_id = try(local.sub_mgmt_nsg.arm_id, var.deployer_tfvars.sub_mgmt_nsg_arm_id)
   sub_mgmt_nsg_exists = length(local.sub_mgmt_nsg_arm_id) > 0 ? true : false
   sub_mgmt_nsg_name = local.sub_mgmt_nsg_exists ? "" : try(
     local.sub_mgmt_nsg.name,
-    var.sub_mgmt_nsg_name != "" ? var.sub_mgmt_nsg_name : format("%s%s", local.prefix, local.resource_suffixes.deployer_subnet_nsg)
+    var.deployer_tfvars.sub_mgmt_nsg_name != "" ? var.deployer_tfvars.sub_mgmt_nsg_name : format("%s%s", local.prefix, local.resource_suffixes.deployer_subnet_nsg)
   )
   sub_mgmt_nsg_allowed_ips = local.sub_mgmt_nsg_exists ? [] : try(
     local.sub_mgmt_nsg.allowed_ips,
-    var.sub_mgmt_nsg_allowed_ips != [] ? var.sub_mgmt_nsg_allowed_ips : ["0.0.0.0/0"]
+    var.deployer_tfvars.sub_mgmt_nsg_allowed_ips != [] ? var.deployer_tfvars.sub_mgmt_nsg_allowed_ips : ["0.0.0.0/0"]
   )
   sub_mgmt_nsg_deployed = try(local.sub_mgmt_nsg_exists ? data.azurerm_network_security_group.nsg_mgmt[0] : azurerm_network_security_group.nsg_mgmt[0], null)
 
@@ -74,48 +77,48 @@ locals {
   // Deployer(s) authentication method with default
   enable_password = contains(compact([
     for deployer in local.deployer_input :
-    try(deployer.authentication.type, var.deployer_authentication_type) == "password" ? true : false
+    try(deployer.authentication.type, var.deployer_tfvars.deployer_authentication_type) == "password" ? true : false
   ]), "true")
 
   // By default use generated password. Provide password under authentication overides it
   input_pwd_list = compact([
     for deployer in local.deployer_input :
-    try(deployer.authentication.password, var.deployer_authentication_password)
+    try(deployer.authentication.password, var.deployer_tfvars.deployer_authentication_password)
   ])
   input_pwd = length(local.input_pwd_list) > 0 ? local.input_pwd_list[0] : null
   password  = (local.enable_deployers && local.enable_password) ? try(local.input_pwd_list[0], random_password.deployer[0].result) : null
 
   enable_key = contains(compact([
     for deployer in local.deployer_input :
-    try(deployer.authentication.type, var.deployer_authentication_type) == "key" ? true : false
+    try(deployer.authentication.type, var.deployer_tfvars.deployer_authentication_type) == "key" ? true : false
   ]), "true")
 
   // By default use generated public key. Provide sshkey.path_to_public_key and path_to_private_key overides it
   public_key = (local.enable_deployers && local.enable_key) ? try(
     file(var.sshkey.path_to_public_key),
-    var.deployer_sshkey_path_to_public_key != "" ? var.deployer_sshkey_path_to_public_key : tls_private_key.deployer[0].public_key_openssh
+    var.deployer_tfvars.deployer_sshkey_path_to_public_key != "" ? var.deployer_tfvars.deployer_sshkey_path_to_public_key : tls_private_key.deployer[0].public_key_openssh
   ) : null
   private_key = (local.enable_deployers && local.enable_key) ? try(
     file(var.sshkey.path_to_private_key),
-    var.deployer_sshkey_path_to_private_key != "" ? var.deployer_sshkey_path_to_private_key : tls_private_key.deployer[0].private_key_pem
+    var.deployer_tfvars.deployer_sshkey_path_to_private_key != "" ? var.deployer_tfvars.deployer_sshkey_path_to_private_key : tls_private_key.deployer[0].private_key_pem
   ) : null
 
   deployers = [
     for idx, deployer in local.deployer_input : {
       "name"                 = local.virtualmachine_names[idx],
       "destroy_after_deploy" = true,
-      "size"                 = try(deployer.size, var.deployer_size),
-      "disk_type"            = try(deployer.disk_type, var.deployer_disk_type)
+      "size"                 = try(deployer.size, var.deployer_tfvars.deployer_size),
+      "disk_type"            = try(deployer.disk_type, var.deployer_tfvars.deployer_disk_type)
       "os" = {
-        "source_image_id" = try(deployer.os.source_image_id, var.deployer_os_source_image_id)
-        "publisher"       = try(deployer.os.source_image_id, "") == "" ? "Canonical" : var.deployer_os.publisher
-        "offer"           = try(deployer.os.source_image_id, "") == "" ? "UbuntuServer" : var.deployer_os.offer
-        "sku"             = try(deployer.os.source_image_id, "") == "" ? "18.04-LTS" : var.deployer_os.sku
-        "version"         = try(deployer.os.source_image_id, "") == "" ? "latest" : var.deployer_os.version
+        "source_image_id" = try(deployer.os.source_image_id, var.deployer_tfvars.deployer_os_source_image_id)
+        "publisher"       = try(deployer.os.source_image_id, "") == "" ? "Canonical" : var.deployer_tfvars.deployer_os.publisher
+        "offer"           = try(deployer.os.source_image_id, "") == "" ? "UbuntuServer" : var.deployer_tfvars.deployer_os.offer
+        "sku"             = try(deployer.os.source_image_id, "") == "" ? "18.04-LTS" : var.deployer_tfvars.deployer_os.sku
+        "version"         = try(deployer.os.source_image_id, "") == "" ? "latest" : var.deployer_tfvars.deployer_os.version
       },
       "authentication" = {
-        "type"     = try(deployer.authentication.type, var.deployer_authentication_type)
-        "username" = try(deployer.authentication.username, var.deployer_authentication_username)
+        "type"     = try(deployer.authentication.type, var.deployer_tfvars.deployer_authentication_type)
+        "username" = try(deployer.authentication.username, var.deployer_tfvars.deployer_authentication_username)
         "sshkey" = {
           "public_key"  = local.public_key
           "private_key" = local.private_key
@@ -129,7 +132,7 @@ locals {
       "private_ip_address" = try(
         deployer.private_ip_address[idx],
         try(
-          var.deployer_private_ip_address[idx], cidrhost(local.sub_mgmt_deployed.address_prefixes[0], idx + 4)
+          var.deployer_tfvars.deployer_private_ip_address[idx], cidrhost(local.sub_mgmt_deployed.address_prefixes[0], idx + 4)
         )
       ),
       "users" = {
