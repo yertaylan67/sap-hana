cc9577d4bc65869493735e3f929234ec6ba74435
Kimmo Forss <kimmo.forss@microsoft.com>
2020-11-18 19:46:29 +0200

    Add a third nic for ANF scaleout

--- deploy/terraform/terraform-units/modules/sap_system/hdb_node/vm-hdb.tf
+++ deploy/terraform/terraform-units/modules/sap_system/hdb_node/vm-hdb.tf
@@ -53,6 +53,33 @@ resource "azurerm_network_interface" "nics_dbnodes_db" {
   }
 }
 
+# Creates the NIC for Hana scaleout
+resource "azurerm_network_interface" "nics_dbnodes_scaleout" {
+  count = local.enable_deployment && local.scaleout_subnet_needed ? length(local.hdb_vms) : 0
+  name  = format("%s%s", local.hdb_vms[count.index].name, local.resource_suffixes.scaleout_nic)
+
+  location                      = var.resource_group[0].location
+  resource_group_name           = var.resource_group[0].name
+  enable_accelerated_networking = true
+
+  ip_configuration {
+    primary   = true
+    name      = "ipconfig1"
+    subnet_id = local.sub_scaleout_exists ? data.azurerm_subnet.scaleout[0].id : azurerm_subnet.scaleout[0].id
+
+    private_ip_address = try(local.hdb_vms[count.index].scaleout_nic_ip, false) != false ? (
+      local.hdb_vms[count.index].scaleout_nic_ip) : (
+      cidrhost(local.sub_scaleout_exists ? (
+        data.azurerm_subnet.scaleout[0].address_prefixes[0]) : (
+        azurerm_subnet.scaleout[0].address_prefixes[0]
+      ), tonumber(count.index) + local.hdb_ip_offsets.hdb_scaleout_vm)
+
+    )
+    private_ip_address_allocation = "static"
+  }
+}
+
+
 # LOAD BALANCER ===================================================================================================
 
 /*-----------------------------------------------------------------------------8
@@ -143,10 +170,13 @@ resource "azurerm_linux_virtual_machine" "vm_dbnode" {
 
   zone = local.enable_ultradisk || local.db_server_count == local.db_zone_count ? local.zones[count.index % max(local.db_zone_count, 1)] : null
 
-  network_interface_ids = [
+  network_interface_ids = local.scaleout_subnet_needed ? ([
     azurerm_network_interface.nics_dbnodes_admin[count.index].id,
-    azurerm_network_interface.nics_dbnodes_db[count.index].id
-  ]
+    azurerm_network_interface.nics_dbnodes_db[count.index].id,
+    azurerm_network_interface.nics_dbnodes_scaleout[count.index].id]) : ([
+    azurerm_network_interface.nics_dbnodes_admin[count.index].id,
+    azurerm_network_interface.nics_dbnodes_db[count.index].id]
+  )
   size                            = lookup(local.sizes, local.hdb_vms[count.index].size).compute.vm_size
   admin_username                  = local.sid_auth_username
   admin_password                  = local.sid_auth_password
