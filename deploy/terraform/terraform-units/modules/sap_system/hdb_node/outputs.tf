50c28c0d5eaee026e402b873678b14d76a10362d
Kimmo Forss <kimmo.forss@microsoft.com>
2020-11-22 16:49:50 +0200

    don't return if not deployed

--- deploy/terraform/terraform-units/modules/sap_system/hdb_node/outputs.tf
+++ deploy/terraform/terraform-units/modules/sap_system/hdb_node/outputs.tf
@@ -21,24 +21,29 @@ output "hana_database_info" {
 
 // Output for DNS
 output "dns_info_vms" {
-  value = concat(
-    flatten([for idx, vm in local.virtualmachine_names :
-      {
-        format("%s%s%s%s", local.prefix, var.naming.separator, vm, local.resource_suffixes.vm) = azurerm_network_interface.nics_dbnodes_admin[idx].private_ip_address
-      }
-    ]), 
-    flatten([for idx, vm in var.naming.virtualmachine_names.HANA_SECONDARY_DNSNAME :
-      {
-        var.naming.virtualmachine_names.HANA_SECONDARY_DNSNAME[idx] = azurerm_network_interface.nics_dbnodes_db[idx].private_ip_address
-      }
-    ])
+  value = local.enable_deployment ? (
+    concat(
+      flatten([for idx, vm in local.virtualmachine_names :
+        {
+          format("%s%s%s%s", local.prefix, var.naming.separator, vm, local.resource_suffixes.vm) = azurerm_network_interface.nics_dbnodes_admin[idx].private_ip_address
+        }
+      ]),
+      flatten([for idx, vm in var.naming.virtualmachine_names.HANA_SECONDARY_DNSNAME :
+        {
+          var.naming.virtualmachine_names.HANA_SECONDARY_DNSNAME[idx] = azurerm_network_interface.nics_dbnodes_db[idx].private_ip_address
+        }
+      ])
+    )) : (
+    null
   )
 }
 
 output "dns_info_loadbalancers" {
-  value = [
-    { format("%s%s%s", local.prefix, var.naming.separator, local.resource_suffixes.db_alb) = azurerm_lb.hdb[0].private_ip_addresses[0] }
-  ]
+  value = local.enable_deployment ? (
+    [{ format("%s%s%s", local.prefix, var.naming.separator, local.resource_suffixes.db_alb) = azurerm_lb.hdb[0].private_ip_addresses[0] }
+    ]) : (
+    null
+  )
 }
 
 
