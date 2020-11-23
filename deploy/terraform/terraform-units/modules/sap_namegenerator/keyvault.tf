c4e238b99ff4bf62a3910e3e002e01f601a4cb04
Kimmo Forss <kimmo.forss@microsoft.com>
2020-11-23 20:37:17 +0200

    PR Comments

--- deploy/terraform/terraform-units/modules/sap_namegenerator/keyvault.tf
+++ deploy/terraform/terraform-units/modules/sap_namegenerator/keyvault.tf
@@ -1,13 +1,13 @@
 locals {
 
-  sdu_private_keyvault_name = format("%s%s%s%sp%s", local.env_verified, local.location_short, local.vnet_verified, var.sap_sid, local.random_id_verified)
-  sdu_user_keyvault_name    = format("%s%s%s%su%s", local.env_verified, local.location_short, local.vnet_verified, var.sap_sid, local.random_id_verified)
+  sdu_private_keyvault_name = format("%s%s%s%sp%s", local.env_verified, local.location_short, local.sap_vnet_verified, var.sap_sid, local.random_id_verified)
+  sdu_user_keyvault_name    = format("%s%s%s%su%s", local.env_verified, local.location_short, local.sap_vnet_verified, var.sap_sid, local.random_id_verified)
 
   deployer_private_keyvault_name = format("%s%s%sprvt%s", local.deployer_env_verified, local.location_short, local.dep_vnet_verified, local.random_id_verified)
   deployer_user_keyvault_name    = format("%s%s%suser%s", local.deployer_env_verified, local.location_short, local.dep_vnet_verified, local.random_id_verified)
 
-  landscape_private_keyvault_name = format("%s%s%sprvt%s", local.landscape_env_verified, local.location_short, local.vnet_verified, local.random_id_verified)
-  landscape_user_keyvault_name    = format("%s%s%suser%s", local.landscape_env_verified, local.location_short, local.vnet_verified, local.random_id_verified)
+  landscape_private_keyvault_name = format("%s%s%sprvt%s", local.landscape_env_verified, local.location_short, local.sap_vnet_verified, local.random_id_verified)
+  landscape_user_keyvault_name    = format("%s%s%suser%s", local.landscape_env_verified, local.location_short, local.sap_vnet_verified, local.random_id_verified)
 
   library_private_keyvault_name = format("%s%sSAPLIBprvt%s", local.env_verified, local.location_short, local.random_id_verified)
   library_user_keyvault_name    = format("%s%sSAPLIBuser%s", local.env_verified, local.location_short, local.random_id_verified)
