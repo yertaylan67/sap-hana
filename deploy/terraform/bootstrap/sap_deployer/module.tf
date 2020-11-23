7bd0e8d183d2aa348b6f8b2341c592e14f028641
Yunzi Zhang <yunzzha@microsoft.com>
2020-11-23 13:09:24 -0800

    bring secrets in existing kv

--- deploy/terraform/bootstrap/sap_deployer/module.tf
+++ deploy/terraform/bootstrap/sap_deployer/module.tf
@@ -4,15 +4,18 @@ Description:
   Example to deploy deployer(s) using local backend.
 */
 module "sap_deployer" {
-  source               = "../../terraform-units/modules/sap_deployer"
-  infrastructure       = var.infrastructure
-  deployers            = var.deployers
-  options              = var.options
-  ssh-timeout          = var.ssh-timeout
-  sshkey               = var.sshkey
-  user_key_vault_id    = var.user_key_vault_id
-  private_key_vault_id = var.private_key_vault_id
-  naming               = module.sap_namegenerator.naming
+  source                  = "../../terraform-units/modules/sap_deployer"
+  infrastructure          = var.infrastructure
+  deployers               = var.deployers
+  options                 = var.options
+  ssh-timeout             = var.ssh-timeout
+  sshkey                  = var.sshkey
+  user_key_vault_id       = var.user_key_vault_id
+  private_key_vault_id    = var.private_key_vault_id
+  public_key_secret_name  = var.public_key_secret_name
+  private_key_secret_name = var.private_key_secret_name
+  password_secret_name    = var.password_secret_name
+  naming                  = module.sap_namegenerator.naming
 }
 
 module "sap_namegenerator" {
